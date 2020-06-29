795753f4369ee880da73f0dbf72aa6dd7d831166
LICHIBOY <1515082318@qq.com>
2020-06-29 14:32:13 +0800

    wyy

---
+++ 2020RS/2020RS/2020RS.h
@@ -0,0 +1,32 @@
+﻿
+// 2020RS.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CMy2020RSApp:
+// 有关此类的实现，请参阅 2020RS.cpp
+//
+
+class CMy2020RSApp : public CWinApp
+{
+public:
+	CMy2020RSApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMy2020RSApp theApp;
